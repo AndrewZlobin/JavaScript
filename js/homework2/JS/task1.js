8131a6d87508d29372c8a68d3d6f323db9073194
AndrewZlobin <andrewzlobin1992@gmail.com>
2019-03-31 11:55:44 +0300

    Remade task №1 as it was explained on lesson

--- js/homework2/JS/task1.js
+++ js/homework2/JS/task1.js
@@ -3,31 +3,16 @@
 Необходимо найти в данном массиве такие два числа M и N, чтобы их сумма была равна 7. Например, 3 + 4 = 7 или 0 + 7 = 7 или -2 + 9 = 7 и тд.
 Для решения достаточно найти хотя бы одну подходящую пару чисел M и N. Подумайте над оптимальным вариантом поиска.*/
 
-arrNumbers = [1,5,7,9,1,5,3,3,3,5,7,7,8,2,1,4,6,9];
+arrNumbers = [1,5,7,9,2,6];
 
-let i = 0;
-let j = 1
-let first = arrNumbers[i];
-let last = arrNumbers[arrNumbers.length - j];
-console.log(first);
-
-while (first !== 3) {
-    i++;
-}
-console.log(first);
-while ((first + last) !== 7) {
-    i++;
-}
-while (first === 1 && last === 6) {
-    console.log(first, last);
-}
-
-/*for (i = 0; i < arrNumbers.length; i++) {
-    if ((first + last) !== 7) {
-        first++;
-        last--;
-    } else {
-        console.log(first, last);
+for (let i = 0; i < arrNumbers.length; i++) {
+    for (let j = i + 1; j < arrNumbers.length; j++) {
+        let sumSeven = arrNumbers[i] + arrNumbers[j];
+        if (sumSeven === 7) {
+            console.log("Подходят следующие числа: " + arrNumbers[i] + " и " + arrNumbers[j]);
+        } else {
+            console.log("Нет подходящих чисел");
+        }
     }
-}*/
+}
 
