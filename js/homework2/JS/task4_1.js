30d5a5d07ff63184873d3ccf82e88c89ee4c0ade
AndrewZlobin <andrewzlobin1992@gmail.com>
2019-03-31 13:55:32 +0300

    Done!

--- js/homework2/JS/task4_1.js
+++ js/homework2/JS/task4_1.js
@@ -1,16 +1,16 @@
 /*Пользователь вводит число, создать массив заданного размера,
 вывести элементы массива в обратном порядке*/
 
+let number = parseInt(prompt('Введите число (таков будет размер массива)'));
 let arrNumbers = [];
+arrNumbers.length = number;
+console.log(arrNumbers);
+let something = 0;
 
-for (let i = 0; i < 6; i++) {
-    let number = prompt('Введите 6 любых чисел');
-    let numberToArr = parseInt(number);
-    arrNumbers.push(numberToArr);
-    console.log(numberToArr);
+for (let i = 0; i < arrNumbers.length; i++) {
+
+    arrNumbers[i] = something;
+    something += 1;
 }
 arrNumbers.reverse();
 console.log(arrNumbers);
-
-/*Массив собирается и ревертится с надеждой на пользователя, который
-* не будет вводить что-либо еще, кроме цифр*/
