aba7c6efbbc925d03a248b42b71053279c0a850d
AndrewZlobin <andrewzlobin1992@gmail.com>
2019-04-02 00:49:31 +0300

    Didn't add "let" for i in "for-loop"

--- js/homework3/JS/task1.js
+++ js/homework3/JS/task1.js
@@ -15,7 +15,7 @@ function arrayToCompare(arrOne, arrTwo) {
         return false;
     }
 
-    for (i = 0; i < arrOne.length; i++) {
+    for (let i = 0; i < arrOne.length; i++) {
         if (arrOne[i] === arrTwo[i]) {
             console.log("Массивы совпадают!");
             return true;
