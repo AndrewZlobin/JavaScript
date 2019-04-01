974245289648f72ef96e37cb517fc1a94277e49b
AndrewZlobin <andrewzlobin1992@gmail.com>
2019-04-02 00:06:53 +0300

    Done task number two

---
+++ js/homework3/JS/task2.js
@@ -0,0 +1,14 @@
+/*Дано натуральное число N.
+Вычислите сумму его цифр, использую рекурсию (строки, массивы и циклы использовать запрещено).*/
+
+function getSumOfNumerals(N) {
+    if (N < 1) {
+        return false;
+    }
+    let variable = 0;
+    variable += (N%10) + getSumOfNumerals(Math.floor(N/10));
+    return variable;
+
+}
+
+console.log("Сумма цифр в числе 3390017 = " + getSumOfNumerals(3390017));
