d24ced8d2aa49ba53be86b3128d1621dd9ae1065
AndrewZlobin <andrewzlobin1992@gmail.com>
2019-04-21 18:17:38 +0300

    Cat fights done!

--- homework9/task3/JS/cat-records.js
+++ homework9/task3/JS/cat-records.js
@@ -1,61 +1,151 @@
-function Cat() {
-    
+class Cat {
+    constructor(name, age, weight, strength) {
+        this._name = name;
+        this._age = age;
+        this._weight = weight;
+        this._strength = strength;
+
+    }
+
+     fight(enemyCat) {
+
+        //Проверка, если вдруг кот будет драться сам с собой
+        if (this._name === enemyCat.name) {
+            console.error("Кот по кличке", this._name, "гоняется за своим хвостом!\nВсем спасибо, расходимся!");
+            return;
+        }
+        //переменные для подсчета очков в конце матча
+        let pointsFirstCat = 0;
+        let pointsSecondCat = 0;
+    //     //Здесь сделать механизм драки
+        console.log("В левом углу ринга", this._name, "\nВ правом углу ринга", enemyCat._name);
+
+        //подсчет очков при сравнении возраста котов
+         //console.error((this._weight - enemyCat.weight) > 4);
+
+        if ((this._age - enemyCat.age) > 5) {
+            console.log("Кот по кличке", this._name, "настолько стар, что теряет два очка!");
+            pointsFirstCat-=2;
+            pointsSecondCat+=2;
+            console.table(this._name, pointsFirstCat,"\t", enemyCat.name, pointsSecondCat);
+        } else if (this._age > enemyCat.age) {
+            console.log("Кот по кличке", this._name, "старше, поэтому теряет 1 очко");
+            pointsFirstCat--;
+            pointsSecondCat++;
+            console.table(this._name, pointsFirstCat, "\t", enemyCat.name, pointsSecondCat);
+        } else if ((enemyCat.age - this._age) > 5) {
+            console.log("Кот по кличке", enemyCat.name, "настолько стар, что теряет два очка!");
+            pointsFirstCat+=2;
+            pointsSecondCat-=2;
+            console.table(this._name, pointsFirstCat,"\t", enemyCat.name, pointsSecondCat);
+        } else if (enemyCat.age > this._age) {
+            console.log("Кот по кличке", enemyCat.name, "старше, поэтому теряет 1 очко");
+            pointsFirstCat++;
+            pointsSecondCat--;
+            console.table(this._name, pointsFirstCat, "\t", enemyCat.name, pointsSecondCat);
+        } else {
+            console.log("Так сказать, возрастная ничья!");
+            console.table(this._name, pointsFirstCat,"\t", enemyCat.name, pointsSecondCat);
+         }
+
+        //подсчет очков по весу
+         if ((this._weight - enemyCat.weight) > 10) {
+             console.log("Кот по кличке", this._name, "настолько огромный, что получает 3 очка!");
+             pointsFirstCat+=3;
+             pointsSecondCat-=3;
+             console.table(this._name, pointsFirstCat,"\t", enemyCat.name, pointsSecondCat);
+         } else if ((this._weight - enemyCat.weight) > 5) {
+             console.log("Кот по кличке", this._name, "намного больше, соответственно, получает 2 очка!");
+             pointsFirstCat+=2;
+             pointsSecondCat-=2;
+             console.table(this._name, pointsFirstCat,"\t", enemyCat.name, pointsSecondCat);
+         } else if (this._weight > enemyCat.weight) {
+             console.log("Кот по кличке", this._name, "больше, и за это ему 1 очко!");
+             pointsFirstCat++;
+             pointsSecondCat--;
+             console.table(this._name, pointsFirstCat, "\t", enemyCat.name, pointsSecondCat);
+         }else if ((enemyCat.weight - this._weight) > 10) {
+             console.log("Кот по кличке", enemyCat.name, "настолько огромный, что получает 3 очка!");
+             pointsFirstCat-=3;
+             pointsSecondCat+=3;
+             console.table(this._name, pointsFirstCat,"\t", enemyCat.name, pointsSecondCat);
+         } else if ((enemyCat.weight - this._weight) > 5) {
+             console.log("Кот по кличке", enemyCat.name, "намного больше, соответственно, получает 2 очка!");
+             pointsFirstCat-=2;
+             pointsSecondCat+=2;
+             console.table(this._name, pointsFirstCat,"\t", enemyCat.name, pointsSecondCat);
+         } else if (enemyCat.weight > this._weight) {
+             console.log("Кот по кличке", this._name, "больше, и за это ему 1 очко!");
+             pointsFirstCat--;
+             pointsSecondCat++;
+             console.table(this._name, pointsFirstCat, "\t", enemyCat.name, pointsSecondCat);
+         } else {
+             console.log("Так сказать, весовая ничья!");
+             console.table(this._name, pointsFirstCat,"\t", enemyCat.name, pointsSecondCat);
+         }
+
+         //подсчет очков по силе
+         if ((this._strength - enemyCat.strength) > 15) {
+             console.log("Кот по кличке", this._name, "настолько сильный, что получает 5 очков!");
+             pointsFirstCat+=5;
+             pointsSecondCat-=5;
+             console.table(this._name, pointsFirstCat,"\t", enemyCat.name, pointsSecondCat);
+         } else if ((this._strength - enemyCat.strength) > 10) {
+             console.log("Кот по кличке", this._name, "намного сильнее, соответственно, получает 3 очка!");
+             pointsFirstCat+=3;
+             pointsSecondCat-=3;
+             console.table(this._name, pointsFirstCat,"\t", enemyCat.name, pointsSecondCat);
+         } else if (this._strength > enemyCat.strength) {
+             console.log("Кот по кличке", this._name, "сильнее, и за это ему 1 очко!");
+             pointsFirstCat++;
+             pointsSecondCat--;
+             console.table(this._name, pointsFirstCat, "\t", enemyCat.name, pointsSecondCat);
+         }else if ((enemyCat.strength - this._strength) > 15) {
+             console.log("Кот по кличке", enemyCat.name, "настолько сильный, что получает 5 очков!");
+             pointsFirstCat-=5;
+             pointsSecondCat+=5;
+             console.table(this._name, pointsFirstCat,"\t", enemyCat.name, pointsSecondCat);
+         } else if ((enemyCat.strength - this._strength) > 10) {
+             console.log("Кот по кличке", enemyCat.name, "намного сильнее, соответственно, получает 3 очка!");
+             pointsFirstCat-=2;
+             pointsSecondCat+=2;
+             console.table(this._name, pointsFirstCat,"\t", enemyCat.name, pointsSecondCat);
+         } else if (enemyCat.strength > this._strength) {
+             console.log("Кот по кличке", this._name, "сильнее, и за это ему 1 очко!");
+             pointsFirstCat--;
+             pointsSecondCat++;
+             console.table(this._name, pointsFirstCat, "\t", enemyCat.name, pointsSecondCat);
+         } else {
+             console.log("Так сказать, ничья!");
+             console.table(this._name, pointsFirstCat,"\t", enemyCat.name, pointsSecondCat);
+         }
+
+         if(pointsFirstCat > pointsSecondCat) {
+             console.log('%c Победил: ',
+                 'color: darkorange',
+                 this._name);
+         } else if (pointsFirstCat < pointsSecondCat) {
+             console.log('%c Победил: ',
+                 'color: darkorange',
+                 enemyCat.name);
+         } else {
+             console.log('%c Победил: ',
+                 'color: darkorange',
+                 "НИЧЬЯ!");
+         }
+
+     }
+
+    get name() {
+        return this._name;
+    }
+    get age() {
+        return this._age;
+    }
+    get weight() {
+        return this._weight;
+    }
+    get strength() {
+        return this._strength;
+    }
 }
-
-Cat.prototype.setName = function (name) {
-    if (name.length < 3) {
-        console.log("Кто так бойцового кота называет? " + this.getName() + "!\nПридумай другое имя");
-        return;
-    }
-
-    this._name = name;
-};
-
-Cat.prototype.getName = function () {
-    return this._name;
-};
-
-/**/
-
-Cat.prototype.setAge = function (age) {
-    if (age < 2) {
-        console.log("Кот " + this.getName() +  "\nслишком молодой! Приходи через годик-другой");
-        return;
-    }
-
-    this._age = age;
-};
-
-Cat.prototype.getAge = function () {
-    return this._age;
-};
-
-/**/
-
-Cat.prototype.setWeight = function (weight) {
-    if (weight < 1 && weight > 10) {
-        console.log("Что с" + this.getName() + "? Коту срочно нужно на диету, никаких боев!");
-        return;
-    }
-
-    this._weight = weight;
-};
-
-Cat.prototype.getWeight = function () {
-    return this._weight;
-};
-
-/**/
-
-Cat.prototype.setStr = function (str) {
-    if (str < 1) {
-        console.log("Твой кот " + this.getName() + " слабак! Не приводи его больше сюда!");
-        return;
-    }
-
-    this._str = str;
-};
-
-Cat.prototype.getStr = function () {
-    return this._str;
-};
--- homework9/task3/JS/start-fight.js
+++ homework9/task3/JS/start-fight.js
@@ -1,45 +1,19 @@
-let cat1 = new Cat();
-cat1.setName("Кот Феликс");
-cat1.setAge(4);
-cat1.setWeight(5);
-cat1.setStr(2);
-//
-// let cat2 = new Cat();
-// cat2.setName("Барон Вискас");
-// cat2.setAge(1);
-// cat2.setWeight(7);
-// //
-// let cat3 = new Cat();
-// cat3.setName("Принцесса");
-// cat3.setAge(8);
-// cat3.setWeight(150);
-//
-let cat4 = new Cat();
-cat4.setName("Мистер Лапкин");
-cat4.setAge(5);
-cat4.setWeight(3);
-cat4.setStr(7);
-//
-// let cat5 = new Cat();
-// cat5.setName("КБ");
-// cat5.setAge(6);
-// cat5.setWeight(6);
+let catNames = ["Мурзик", "Васька", "Феликс",
+    "Алиса", "Дайра", "Бездомный",
+    "Том", "Дракоша", "Манчестер",
+    "Босс"];
 
-////////
+function randomStats(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 
-console.log(cat1);
-// console.log(cat2);
-// console.log(cat3);
-console.log(cat4);
-// console.log(cat5);
+//let item = items[Math.floor(Math.random()*items.length)];
+//words[Math.floor(Math.random()*words.length)];
 
-let randomRound = new FightEngine("CatUnity", "0.2", 2);
-randomRound.catsToFight(cat1);
-// randomRound.catsToFight(cat2);
-// randomRound.catsToFight(cat3);
-randomRound.catsToFight(cat4);
-// randomRound.catsToFight(cat5);
+let firstCat = new Cat(catNames[Math.floor(Math.random()*catNames.length)], randomStats(1, 15), randomStats(1, 20), randomStats(1, 30));
+let secondCat = new Cat(catNames[Math.floor(Math.random()*catNames.length)], randomStats(1, 15), randomStats(1, 20), randomStats(1, 30));
 
-randomRound.fightResults();
+console.log(firstCat);
+console.log(secondCat);
 
-console.log(randomRound);
+firstCat.fight(secondCat);
