b2636fdf313290198d94f1a59fca4b20f3475e5d
AndrewZlobin <andrewzlobin1992@gmail.com>
2019-04-24 22:05:17 +0300

    Some refactoring to code, works fine

--- homework8/JS/task2-forms.js
+++ homework8/JS/task2-forms.js
@@ -8,7 +8,8 @@ form.elements.checkbox_fieldset.addEventListener("click", changeColorOfFieldset)
 
 function changeColorOfFieldset() {
     document.getElementsByName("radiocolor").checked = true;
-    form.elements.checkbox_fieldset.style.background = colorRadio.value;
+    form.elements.checkbox_fieldset.style.background = this.value;
+    console.log(this);
 }
 
 //Отображение выбранных ЯП в консоли
@@ -35,6 +36,9 @@ function addProgLangsToConsole() {
 form.elements.checkbox_fieldset.addEventListener('click', turnUnDisabled);
 
 function turnUnDisabled(event) {
+    //супер-короткая запись
+    //form.elements.disable.disabled = !this.checked;
+
     let eventTarget = event.target;
     //console.log(eventTarget);
     let turnDisabled = form.elements.textarea;
