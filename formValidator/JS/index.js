aca9ae56b0a6b68854559eeed048a27828d5d831
AndrewZlobin <andrewzlobin1992@gmail.com>
2019-05-05 14:56:38 +0300

    Create something like form validator

---
+++ formValidator/JS/index.js
@@ -0,0 +1,29 @@
+let form = document.forms.someForm;
+console.log(form);
+let formValidator = new FormValidator(form);
+console.log(formValidator);
+
+formValidator.addRules({
+    rules: {
+        login: /^[a-zA-Z0-9]+$/,
+        pwd: /^[a-zA-Z0-9]+$/
+    },
+    messages: {
+        login: "Логин должен представлять из себя набор букв и цифр, но можно использовать только латиницу!",
+        pwd: "Пароль должен представлять из себя набор латинских букв и цифр!"
+    }
+});
+
+form.addEventListener("submit", noErorrs);
+
+function noErorrs() {
+    if (formValidator.isValid()) {
+        alert("Всё хорошо");
+
+    }
+}
+
+console.log(formValidator._err);
+
+// formValidator.some();
+// formValidator.validate();
---
+++ formValidator/JS/mainScript.js
@@ -0,0 +1,62 @@
+class FormValidator{
+    constructor(form) {
+        this._form = form;
+        this._validElems = document.querySelectorAll(".validate");
+        this._form.addEventListener('submit', this.checkSome.bind(this));
+        this._err = [];
+        console.log(this._validElems[0].placeholder);
+    };
+    addRules(rules){
+        this._rules = rules.rules;
+        this._messages = rules.messages;
+        // console.log(this._rules);
+        // console.log(this._rules[this._validElems[0].name].test(this._validElems[0].value));
+    };
+
+    checkSome(event) {
+        event.preventDefault();
+
+        // let errorForLogin = document.getElementsByClassName("error-for-login");
+        // let errorForPwd = document.getElementsByClassName("error-for-pwd");
+        // console.warn(errorForLogin, errorForPwd);
+
+        for (let i = 0; i < this._validElems.length; i++){
+            if (!this._rules[this._validElems[i].name].test(this._validElems[i].value)) {
+                this._err.push([this._validElems[i].name]);
+                console.log(this._err);
+                console.log(this._validElems[i].name);
+            }
+
+            if (this._validElems[i].name === 'pwd' &&
+                !this._rules[this._validElems[i].name].test(this._validElems[i].value)){
+                    alert(this._validElems[i].name + " is incorrect!\n" + this._messages.pwd);
+                    // console.log(errorForPwd);
+                }
+
+            if (this._validElems[i].name === 'login' &&
+                !this._rules[this._validElems[i].name].test(this._validElems[i].value)){
+                alert(this._validElems[i].name + " is incorrect!\n" + this._messages.login);
+                // console.log(this._validElems[i].name)
+            }
+
+            // if (!this._rules[this._validElems[i].name].test(this._validElems[i].value)){
+            //     alert(this._validElems[i].name + " is incorrect!");
+            // }
+                // if (!this._rules[this._validElems[i].name].test(this._validElems[i].value)){
+                //     // errorForPwd.innerHTML = this._messages.pwd;
+                //     console.log(this._validElems[i].name);
+                //     alert(this._validElems[i].name + " is incorrect!");
+                // }
+        }
+    };
+
+    isValid() {
+        if (this._err.length > 0) {
+            console.warn("Ошибок:", this._err.length);
+            return false;
+        } else {
+            return true;
+        }
+
+    };
+}
